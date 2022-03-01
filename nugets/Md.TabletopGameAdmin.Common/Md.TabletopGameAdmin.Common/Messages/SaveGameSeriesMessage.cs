dc1986b27d058569272dee9d4ba13b7d1ecb2b3a
Michael Diers <md@sreid.de>
2022-03-01 23:45:01 +0100

    Md.TabletopGameAdmin.Common: internalId added

--- nugets/Md.TabletopGameAdmin.Common/Md.TabletopGameAdmin.Common/Messages/SaveGameSeriesMessage.cs
+++ nugets/Md.TabletopGameAdmin.Common/Md.TabletopGameAdmin.Common/Messages/SaveGameSeriesMessage.cs
@@ -17,11 +17,23 @@
         /// </summary>
         /// <param name="processId">The id of the process.</param>
         /// <param name="gameSeries">The data of the new game series.</param>
+        /// <param name="internalId">The internal id.</param>
         /// <exception cref="ArgumentNullException">Is thrown if <paramref name="gameSeries" /> is null.</exception>
-        public SaveGameSeriesMessage(string processId, IGameSeries gameSeries)
+        public SaveGameSeriesMessage(string processId, IGameSeries gameSeries, string internalId)
             : base(processId)
         {
+            if (string.IsNullOrWhiteSpace(internalId))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(internalId));
+            }
+
+            if (!Guid.TryParse(internalId, out var guid) || guid == Guid.Empty)
+            {
+                throw new ArgumentException("Value is not a valid guid.", nameof(internalId));
+            }
+
             this.GameSeries = gameSeries ?? throw new ArgumentNullException(nameof(gameSeries));
+            this.InternalId = internalId;
         }
 
         /// <summary>
@@ -29,12 +41,19 @@
         /// </summary>
         /// <param name="processId">The id of the process.</param>
         /// <param name="gameSeries">The data of the new game series.</param>
+        /// <param name="internalId">The internal id.</param>
         /// <exception cref="ArgumentNullException">Is thrown if <paramref name="gameSeries" /> is null.</exception>
         [JsonConstructor]
-        public SaveGameSeriesMessage(string processId, GameSeries gameSeries)
-            : this(processId, gameSeries as IGameSeries)
+        public SaveGameSeriesMessage(string processId, GameSeries gameSeries, string internalId)
+            : this(processId, gameSeries as IGameSeries, internalId)
         {
         }
+
+        /// <summary>
+        ///     Get the data the internal id.
+        /// </summary>
+        [JsonProperty("internalId", Required = Required.Always, Order = 12)]
+        public string InternalId { get; }
 
         /// <summary>
         ///     Get the data of the game series.
