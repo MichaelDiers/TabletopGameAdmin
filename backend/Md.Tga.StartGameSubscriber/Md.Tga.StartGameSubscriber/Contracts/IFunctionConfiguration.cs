41c78a7477d938abca44f4b98df626de471d7240
Michael Diers <md@sreid.de>
2022-03-08 22:34:38 +0100

    Md.Tga.StartGameSubscriber: add logic

--- backend/Md.Tga.StartGameSubscriber/Md.Tga.StartGameSubscriber/Contracts/IFunctionConfiguration.cs
+++ backend/Md.Tga.StartGameSubscriber/Md.Tga.StartGameSubscriber/Contracts/IFunctionConfiguration.cs
@@ -16,8 +16,23 @@
         string GameSeriesCollectionName { get; }
 
         /// <summary>
+        ///     Gets the name of to topic initialize survey.
+        /// </summary>
+        string InitializeSurveyTopicName { get; }
+
+        /// <summary>
         ///     Gets the project id.
         /// </summary>
         string ProjectId { get; }
+
+        /// <summary>
+        ///     Gets the name of the translations collection.
+        /// </summary>
+        string TranslationsCollectionName { get; }
+
+        /// <summary>
+        ///     Gets the id of the translations document.
+        /// </summary>
+        string TranslationsDocument { get; }
     }
 }
--- backend/Md.Tga.StartGameSubscriber/Md.Tga.StartGameSubscriber/Logic/FunctionProvider.cs
+++ backend/Md.Tga.StartGameSubscriber/Md.Tga.StartGameSubscriber/Logic/FunctionProvider.cs
@@ -1,12 +1,19 @@
 ﻿namespace Md.Tga.StartGameSubscriber.Logic
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using Md.GoogleCloud.Base.Contracts.Logic;
     using Md.GoogleCloud.Base.Logic;
     using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Models;
     using Md.Tga.StartGameSubscriber.Contracts;
     using Microsoft.Extensions.Logging;
+    using Surveys.Common.Contracts;
+    using Surveys.Common.Messages;
+    using Surveys.Common.Models;
+    using Person = Surveys.Common.Models.Person;
 
     /// <summary>
     ///     Provider that handles the business logic of the cloud function.
@@ -24,20 +31,37 @@
         private readonly IReadOnlyDatabase gameSeriesDatabase;
 
         /// <summary>
+        ///     Access pub/sub.
+        /// </summary>
+        private readonly IPubSubClient pubSubClient;
+
+        /// <summary>
+        ///     Access to the database collection translations.
+        /// </summary>
+        private readonly ITranslationsReadOnlyDatabase translationsDatabase;
+
+        /// <summary>
         ///     Creates a new instance of <see cref="FunctionProvider" />.
         /// </summary>
         /// <param name="logger">An error logger.</param>
         /// <param name="gameSeriesDatabase">Access to the database collection game-series.</param>
         /// <param name="gamesDatabase">Access to the database collection games.</param>
+        /// <param name="translationsDatabase">Access to the database collection translations.</param>
+        /// <param name="pubSubClient">Client for accessing pub/sub.</param>
         public FunctionProvider(
             ILogger<Function> logger,
             IGameSeriesReadOnlyDatabase gameSeriesDatabase,
-            IGamesReadOnlyDatabase gamesDatabase
+            IGamesReadOnlyDatabase gamesDatabase,
+            ITranslationsReadOnlyDatabase translationsDatabase,
+            IPubSubClient pubSubClient
         )
             : base(logger)
         {
             this.gameSeriesDatabase = gameSeriesDatabase ?? throw new ArgumentNullException(nameof(gameSeriesDatabase));
             this.gamesDatabase = gamesDatabase ?? throw new ArgumentNullException(nameof(gamesDatabase));
+            this.translationsDatabase =
+                translationsDatabase ?? throw new ArgumentNullException(nameof(translationsDatabase));
+            this.pubSubClient = pubSubClient ?? throw new ArgumentNullException(nameof(pubSubClient));
         }
 
         /// <summary>
@@ -45,10 +69,156 @@
         /// </summary>
         /// <param name="message">The message that is handled.</param>
         /// <returns>A <see cref="Task" />.</returns>
-        protected override Task HandleMessageAsync(IStartGameMessage message)
+        protected override async Task HandleMessageAsync(IStartGameMessage message)
         {
-            //await this.database.InsertAsync(message.InternalId, message.GameSeries);
-            return Task.CompletedTask;
+            var gameSeries = await this.ReadGameSeries(message);
+
+            var surveyName = string.Empty;
+            var surveyInfo = string.Empty;
+            var surveyInfoLink = string.Empty;
+            var answerDefault = string.Empty;
+            var questions = new[]
+            {
+                string.Empty,
+                string.Empty,
+                string.Empty
+            };
+
+            var translations = await this.translationsDatabase.ReadGermanTranslations();
+            if (translations != null)
+            {
+                if (translations.ContainsKey("survey-name"))
+                {
+                    surveyName = translations["survey-name"];
+                }
+
+                if (translations.ContainsKey("survey-info"))
+                {
+                    surveyInfo = translations["survey-info"];
+                }
+
+                if (translations.ContainsKey("survey-info-link"))
+                {
+                    surveyInfoLink = translations["survey-info-link"];
+                }
+
+                if (translations.ContainsKey("answer-default"))
+                {
+                    answerDefault = translations["answer-default"];
+                }
+
+                if (translations.ContainsKey("survey-question-0"))
+                {
+                    questions[0] = translations["survey-question-0"];
+                }
+
+                if (translations.ContainsKey("survey-question-1"))
+                {
+                    questions[0] = translations["survey-question-1"];
+                }
+
+                if (translations.ContainsKey("survey-question-2"))
+                {
+                    questions[0] = translations["survey-question-2"];
+                }
+            }
+
+            var initializeSurveyMessage = this.BuildInitializeSurveyMessage(
+                message.ProcessId,
+                gameSeries,
+                surveyName,
+                surveyInfo,
+                surveyInfoLink,
+                answerDefault,
+                questions);
+            await this.pubSubClient.PublishAsync(initializeSurveyMessage);
+        }
+
+        private IInitializeSurveyMessage BuildInitializeSurveyMessage(
+            string processId,
+            IGameSeries gameSeries,
+            string surveyName,
+            string surveyInfo,
+            string surveyInfoLink,
+            string answerDefault,
+            string[] questionTexts
+        )
+        {
+            var choices = gameSeries.Countries.Select(
+                (country, i) => new Choice(
+                    Guid.NewGuid().ToString(),
+                    country.Name,
+                    true,
+                    i + 1));
+            choices = choices.Prepend(
+                new Choice(
+                    Guid.NewGuid().ToString(),
+                    answerDefault,
+                    false,
+                    0)).ToArray();
+
+            var questions = new[]
+            {
+                new Question(
+                    Guid.NewGuid().ToString(),
+                    questionTexts[0],
+                    choices,
+                    0),
+                new Question(
+                    Guid.NewGuid().ToString(),
+                    questionTexts[1],
+                    choices,
+                    0),
+                new Question(
+                    Guid.NewGuid().ToString(),
+                    questionTexts[2],
+                    choices,
+                    0)
+            };
+
+            var participants = gameSeries.Players.Select(
+                (player, i) => new Participant(
+                    player.Id,
+                    player.Email,
+                    player.Name,
+                    questions.Select(
+                        question => new QuestionReference(
+                            question.Id,
+                            question.Choices.First(choice => choice.Order == 0).Id)).ToArray(),
+                    i));
+
+            return new InitializeSurveyMessage(
+                new Survey(
+                    Guid.NewGuid().ToString(),
+                    surveyName,
+                    surveyInfo,
+                    surveyInfoLink,
+                    new Person(gameSeries.Organizer.Id, gameSeries.Organizer.Email, gameSeries.Organizer.Name),
+                    participants,
+                    questions),
+                processId);
+        }
+
+        /// <summary>
+        ///     Returns the <see cref="IStartGameMessage.GameSeries" /> or reads the data from the database using
+        ///     <see cref="IStartGameMessage.InternalId" />.
+        /// </summary>
+        /// <param name="message">The incoming message of the pub/sub function.</param>
+        /// <returns>The game series for that a new game will be started.</returns>
+        private async Task<IGameSeries> ReadGameSeries(IStartGameMessage message)
+        {
+            if (message.GameSeries != null)
+            {
+                return message.GameSeries;
+            }
+
+            var dictionary = await this.gameSeriesDatabase.ReadByDocumentIdAsync(message.InternalId);
+            if (dictionary == null)
+            {
+                throw new InvalidOperationException($"Unknown game series internal id: {message.InternalId}");
+            }
+
+            return GameSeries.FromDictionary(dictionary);
         }
     }
 }
