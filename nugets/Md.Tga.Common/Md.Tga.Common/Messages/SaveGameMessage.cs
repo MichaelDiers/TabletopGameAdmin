e06d710b815da6d73e7d165ead9da2fbe4d43f5a
Michael Diers <md@sreid.de>
2022-03-09 17:54:03 +0100

    Md.Tga.Common: json fix

--- nugets/Md.Tga.Common/Md.Tga.Common/Messages/SaveGameMessage.cs
+++ nugets/Md.Tga.Common/Md.Tga.Common/Messages/SaveGameMessage.cs
@@ -4,6 +4,7 @@
     using Md.GoogleCloud.Base.Messages;
     using Md.Tga.Common.Contracts.Messages;
     using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Models;
     using Newtonsoft.Json;
 
     /// <summary>
@@ -11,6 +12,17 @@
     /// </summary>
     public class SaveGameMessage : Message, ISaveGameMessage
     {
+        /// <summary>
+        ///     Creates a new instance of <see cref="SaveGameMessage" />.
+        /// </summary>
+        /// <param name="processId">The global process id.</param>
+        /// <param name="game">The game to be saved.</param>
+        [JsonConstructor]
+        public SaveGameMessage(string processId, Game game)
+            : this(processId, game as IGame)
+        {
+        }
+
         /// <summary>
         ///     Creates a new instance of <see cref="SaveGameMessage" />.
         /// </summary>
