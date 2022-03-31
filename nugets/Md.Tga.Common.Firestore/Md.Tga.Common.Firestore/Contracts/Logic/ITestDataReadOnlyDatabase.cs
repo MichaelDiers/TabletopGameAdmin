afc61d0c8bbbdd7a400ef3bc1ab15f53c331a075
MichaelDiers <md@sreid.de>
2022-04-01 01:13:50 +0200

    access test data

---
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Contracts/Logic/ITestDataReadOnlyDatabase.cs
@@ -0,0 +1,17 @@
+﻿namespace Md.Tga.Common.Firestore.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Models;
+
+    /// <summary>
+    ///     Read test data from database.
+    /// </summary>
+    public interface ITestDataReadOnlyDatabase
+    {
+        /// <summary>
+        ///     Read test data for game series.
+        /// </summary>
+        /// <returns>A <see cref="Task" /> whose result is a <see cref="IGameSeries" />.</returns>
+        Task<IGameSeries> ReadGameSeriesAsync();
+    }
+}
---
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Logic/TestDataReadOnlyDatabase.cs
@@ -0,0 +1,71 @@
+﻿namespace Md.Tga.Common.Firestore.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Md.Common.Contracts;
+    using Md.Common.Extensions;
+    using Md.GoogleCloud.Base.Contracts.Logic;
+    using Md.GoogleCloud.Base.Logic;
+    using Md.GoogleCloudFirestore.Logic;
+    using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Firestore.Contracts.Logic;
+    using Md.Tga.Common.Models;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    ///     Read test data from gameSeriesDatabase.
+    /// </summary>
+    public class TestDataReadOnlyDatabase : ITestDataReadOnlyDatabase
+    {
+        /// <summary>
+        ///     Name of the test data collection.
+        /// </summary>
+        private const string CollectionName = "test-data";
+
+        /// <summary>
+        ///     Id of the game series document.
+        /// </summary>
+        private const string GameSeriesDocumentId = "game-series";
+
+        /// <summary>
+        ///     The id of the json field containing game series data.
+        /// </summary>
+        private const string GameSeriesJsonId = "json";
+
+        /// <summary>
+        ///     Access the test-data database.
+        /// </summary>
+        private readonly IDatabase<IDictionary<string, object>> gameSeriesDatabase;
+
+        /// <summary>
+        ///     Creates a new instance of <see cref="TestDataReadOnlyDatabase" />.
+        /// </summary>
+        /// <param name="environment">The runtime environment.</param>
+        public TestDataReadOnlyDatabase(IRuntimeEnvironment environment)
+        {
+            this.gameSeriesDatabase = new Database<IDictionary<string, object>>(
+                new DatabaseConfiguration(environment.ProjectId, TestDataReadOnlyDatabase.CollectionName),
+                x => x);
+        }
+
+        /// <summary>
+        ///     Read test data for game series.
+        /// </summary>
+        /// <returns>A <see cref="Task" /> whose result is a <see cref="IGameSeries" />.</returns>
+        public async Task<IGameSeries> ReadGameSeriesAsync()
+        {
+            var data = JsonConvert.DeserializeObject<GameSeries>(
+                (await this.gameSeriesDatabase.ReadByDocumentIdAsync(TestDataReadOnlyDatabase.GameSeriesDocumentId))
+                ?.GetString(TestDataReadOnlyDatabase.GameSeriesJsonId) ??
+                string.Empty);
+            if (data == null)
+            {
+                throw new Exception(
+                    $"Cannot read game series data for document id {TestDataReadOnlyDatabase.GameSeriesDocumentId}");
+            }
+
+            return data;
+        }
+    }
+}
