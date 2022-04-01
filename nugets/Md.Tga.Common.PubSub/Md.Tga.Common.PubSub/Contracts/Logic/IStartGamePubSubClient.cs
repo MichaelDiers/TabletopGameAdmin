c0143ac5f74caa2067473b5cfe6bc445cbd598d5
MichaelDiers <md@sreid.de>
2022-04-01 11:46:25 +0200

    Md.Tga.Common.PubSub: handle start game

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Contracts/Logic/IStartGamePubSubClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Md.Tga.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IStartGameMessage" />.
+    /// </summary>
+    public interface IStartGamePubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="IStartGameMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        // ReSharper disable once UnusedMember.Global
+        Task PublishAsync(IStartGameMessage message);
+    }
+}
---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/StartGamePubSubClient.cs
@@ -0,0 +1,20 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IStartGameMessage" />.
+    /// </summary>
+    public class StartGamePubSubClient : AbstractPubSubClient<IStartGameMessage>, IStartGamePubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="StartGamePubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public StartGamePubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
