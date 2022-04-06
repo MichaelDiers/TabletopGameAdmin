dbb2d040fdc26fa6fa323ace3e1d80bbff615bd2
MichaelDiers <md@sreid.de>
2022-04-06 21:11:03 +0200

    publish messages for saving player mappings

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/SavePlayerMappingsPubSubClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISavePlayerMappingsMessage" />.
+    /// </summary>
+    public class SavePlayerMappingsPubSubClient
+        : AbstractPubSubClient<ISavePlayerMappingsMessage>, ISavePlayerMappingsPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="SavePlayerMappingsPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public SavePlayerMappingsPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
