e55c17a893445127f46972feae41555c6f0d0b4e
MichaelDiers <md@sreid.de>
2022-04-11 19:25:29 +0200

    refactoring and fixes

---
+++ backend/Md.Tga.SaveGameSeriesSubscriber/Md.Tga.SaveGameSeriesSubscriber.Tests/Logic/FunctionProviderTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Md.Tga.SaveGameSeriesSubscriber.Tests.Logic
+{
+    using System;
+    using Google.Cloud.Functions.Testing;
+    using Md.Common.Logic;
+    using Md.Common.Model;
+    using Md.Tga.Common.Firestore.Logic;
+    using Md.Tga.Common.Messages;
+    using Md.Tga.Common.Models;
+    using Md.Tga.Common.TestData.Generators;
+    using Md.Tga.Common.TestData.Mocks.Database;
+    using Md.Tga.Common.TestData.Mocks.PubSub;
+    using Md.Tga.SaveGameSeriesSubscriber.Logic;
+    using Xunit;
+    using Environment = Md.Common.Contracts.Model.Environment;
+
+    public class FunctionProviderTests
+    {
+        [Fact]
+        public async void HandleAsync()
+        {
+            var testData = new TestDataContainer();
+            var gameSeries = new GameSeries(
+                null,
+                null,
+                testData.GameSeries.Name,
+                testData.GameSeries.Sides,
+                testData.GameSeries.Countries,
+                testData.GameSeries.Organizer,
+                testData.GameSeries.Players,
+                testData.GameSeries.GameType);
+            var saveGameSeriesMessage = new SaveGameSeriesMessage(Guid.NewGuid().ToString(), gameSeries);
+            var logger = new MemoryLogger<Function>();
+            var provider = new FunctionProvider(logger, new GameSeriesDatabaseMock(), new StartGamePubSubClientMock());
+            await provider.HandleAsync(saveGameSeriesMessage);
+            Assert.Empty(logger.ListLogEntries());
+        }
+
+        [Theory(Skip = "Integration")]
+        [InlineData(Environment.Test, "projectId")]
+        public async void HandleAsyncIntegration(Environment environment, string projectId)
+        {
+            var testData = new TestDataContainer();
+            var gameSeries = new GameSeries(
+                null,
+                null,
+                testData.GameSeries.Name,
+                testData.GameSeries.Sides,
+                testData.GameSeries.Countries,
+                testData.GameSeries.Organizer,
+                testData.GameSeries.Players,
+                testData.GameSeries.GameType);
+            var saveGameSeriesMessage = new SaveGameSeriesMessage(Guid.NewGuid().ToString(), gameSeries);
+            var json = Serializer.SerializeObject(saveGameSeriesMessage);
+            saveGameSeriesMessage = Serializer.DeserializeObject<SaveGameSeriesMessage>(json);
+            var logger = new MemoryLogger<Function>();
+            var provider = new FunctionProvider(
+                logger,
+                new GameSeriesDatabase(new RuntimeEnvironment {Environment = environment, ProjectId = projectId}),
+                new StartGamePubSubClientMock());
+            await provider.HandleAsync(saveGameSeriesMessage);
+            Assert.Empty(logger.ListLogEntries());
+        }
+    }
+}
