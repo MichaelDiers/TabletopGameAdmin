bdedc1805d554e984fe1a3298c646e4ea74ab2ef
MichaelDiers <md@sreid.de>
2022-04-12 22:40:38 +0200

    nuget update

---
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore.Tests/Logic/TestDataReadOnlyDatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Md.Tga.Common.Firestore.Tests.Logic
+{
+    using Md.Common.Contracts.Model;
+    using Md.Common.Model;
+    using Md.Tga.Common.Firestore.Logic;
+    using Xunit;
+
+    public class TestDataReadOnlyDatabaseTests
+    {
+        [Theory(Skip = "Integration")]
+        [InlineData("projectId")]
+        public async void ReadStartGameSeriesMessageAsync(string projectId)
+        {
+            var database = new TestDataReadOnlyDatabase(
+                new RuntimeEnvironment {Environment = Environment.Test, ProjectId = projectId});
+            var message = await database.ReadStartGameSeriesMessageAsync();
+            Assert.NotNull(message);
+        }
+    }
+}
