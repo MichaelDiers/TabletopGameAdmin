3238b758918e43f76d61e754d4bb178191c15dce
MichaelDiers <md@sreid.de>
2022-04-12 23:24:46 +0200

    test data added

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/StartGameSeriesPubSubClientMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    public class StartGameSeriesPubSubClientMock
+        : PubSubClientMock<IStartGameSeriesMessage>, IStartGameSeriesPubSubClient
+    {
+        public StartGameSeriesPubSubClientMock()
+        {
+        }
+
+        public StartGameSeriesPubSubClientMock(IStartGameSeriesMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+        protected override Task<bool> CheckMessage(IStartGameSeriesMessage expected, IStartGameSeriesMessage actual)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
