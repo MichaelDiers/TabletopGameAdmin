9510b86255749f2202e34099d647a25c867194db
MichaelDiers <md@sreid.de>
2022-04-12 21:33:26 +0200

    new pub/sub client

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/StartGameSeriesPubSubClient.cs
@@ -0,0 +1,23 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Contracts.Model;
+    using Md.GoogleCloudPubSub.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IStartGameSeriesMessage" />.
+    /// </summary>
+    public class StartGameSeriesPubSubClient
+        : AbstractPubSubClient<IStartGameSeriesMessage>, IStartGameSeriesPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="StartGameSeriesPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public StartGameSeriesPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
