cb62e46fd6a6894e5d0dfb46ac0a9181eae71e20
MichaelDiers <md@sreid.de>
2022-04-12 19:19:21 +0200

    add db mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/Database/GameConfigDatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.Database
+{
+    using System.Collections.Generic;
+    using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Firestore.Contracts.Logic;
+    using Md.Tga.Common.Models;
+
+    public class GameConfigDatabaseMock : DatabaseMock<IGameConfig>, IGameConfigDatabase
+    {
+        public GameConfigDatabaseMock()
+            : this(new Dictionary<string, IGameConfig>())
+        {
+        }
+
+        public GameConfigDatabaseMock(IDictionary<string, IGameConfig> dictionary)
+            : base(dictionary, GameConfig.FromDictionary)
+        {
+        }
+    }
+}
