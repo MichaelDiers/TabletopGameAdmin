e278b1c38990adde33d18eadb91cec2d5a409785
MichaelDiers <md@sreid.de>
2022-04-14 20:36:48 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/StartSurveyPubSubClientMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    public class StartSurveyPubSubClientMock : PubSubClientMock<IStartSurveyMessage>, IStartSurveyPubSubClient
+    {
+        public StartSurveyPubSubClientMock()
+        {
+        }
+
+        public StartSurveyPubSubClientMock(IStartSurveyMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+        protected override async Task<bool> CheckMessage(IStartSurveyMessage expected, IStartSurveyMessage actual)
+        {
+            await Task.CompletedTask;
+            return true;
+        }
+    }
+}
