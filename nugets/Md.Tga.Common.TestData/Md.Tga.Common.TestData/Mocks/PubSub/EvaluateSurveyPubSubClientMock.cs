9ba12b0068aa922cc2b38ea5e4c913cc2563b20e
MichaelDiers <md@sreid.de>
2022-04-14 23:45:06 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/EvaluateSurveyPubSubClientMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    public class EvaluateSurveyPubSubClientMock : PubSubClientMock<IEvaluateSurveyMessage>, IEvaluateSurveyPubSubClient
+    {
+        public EvaluateSurveyPubSubClientMock()
+        {
+        }
+
+        public EvaluateSurveyPubSubClientMock(IEvaluateSurveyMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+        protected override async Task<bool> CheckMessage(IEvaluateSurveyMessage expected, IEvaluateSurveyMessage actual)
+        {
+            await Task.CompletedTask;
+            return true;
+        }
+    }
+}
