c2d9ed724c0ff00e57a964bdb5ca35f61ccf3270
MichaelDiers <md@sreid.de>
2022-04-14 20:31:37 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/SaveGamePubSubClientMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    public class SaveGamePubSubClientMock : PubSubClientMock<ISaveGameMessage>, ISaveGamePubSubClient
+    {
+        public SaveGamePubSubClientMock()
+        {
+        }
+
+        public SaveGamePubSubClientMock(ISaveGameMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+        protected override async Task<bool> CheckMessage(ISaveGameMessage expected, ISaveGameMessage actual)
+        {
+            await Task.CompletedTask;
+            return true;
+        }
+    }
+}
