0e1e9ea3ec27c9b79ca0169a40933a668c1c6e4a
MichaelDiers <md@sreid.de>
2022-04-14 20:07:48 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/SaveSurveyPubSubClientMock.cs
@@ -0,0 +1,26 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    public class SaveSurveyPubSubClientMock : PubSubClientMock<ISaveSurveyMessage>, ISaveSurveyPubSubClient
+    {
+        public SaveSurveyPubSubClientMock()
+
+        {
+        }
+
+        public SaveSurveyPubSubClientMock(ISaveSurveyMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+
+        protected override async Task<bool> CheckMessage(ISaveSurveyMessage expected, ISaveSurveyMessage actual)
+        {
+            await Task.CompletedTask;
+            return true;
+        }
+    }
+}
