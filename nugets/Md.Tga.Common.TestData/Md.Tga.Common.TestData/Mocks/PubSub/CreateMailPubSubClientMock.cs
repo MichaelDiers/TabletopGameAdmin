f23edc1a21ccdca5a0927a6477075344d02b7881
MichaelDiers <md@sreid.de>
2022-04-14 22:32:19 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/PubSub/CreateMailPubSubClientMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.PubSub
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    public class CreateMailPubSubClientMock : PubSubClientMock<ICreateMailMessage>, ICreateMailPubSubClient
+    {
+        public CreateMailPubSubClientMock()
+        {
+        }
+
+        public CreateMailPubSubClientMock(ICreateMailMessage? expectedMessage)
+            : base(expectedMessage)
+        {
+        }
+
+        protected override async Task<bool> CheckMessage(ICreateMailMessage expected, ICreateMailMessage actual)
+        {
+            await Task.CompletedTask;
+            return true;
+        }
+    }
+}
