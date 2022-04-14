666e93f49e719fbb58f5e4f492fe090de1180233
MichaelDiers <md@sreid.de>
2022-04-14 14:40:34 +0200

    new pub/sub client

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/StartSurveyPubSubClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Contracts.Model;
+    using Md.GoogleCloudPubSub.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IStartSurveyMessage" />.
+    /// </summary>
+    public class StartSurveyPubSubClient : AbstractPubSubClient<IStartSurveyMessage>, IStartSurveyPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="StartSurveyPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public StartSurveyPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
