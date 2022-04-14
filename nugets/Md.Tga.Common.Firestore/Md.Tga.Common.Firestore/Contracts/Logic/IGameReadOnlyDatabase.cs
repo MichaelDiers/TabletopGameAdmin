fc1252c21b8c23a549d5d692fd9559cfcfc0467d
MichaelDiers <md@sreid.de>
2022-04-14 12:21:00 +0200

    count games

--- nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Contracts/Logic/IGameReadOnlyDatabase.cs
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Contracts/Logic/IGameReadOnlyDatabase.cs
@@ -1,5 +1,6 @@
 ﻿namespace Md.Tga.Common.Firestore.Contracts.Logic
 {
+    using System.Threading.Tasks;
     using Md.GoogleCloudFirestore.Contracts.Logic;
     using Md.Tga.Common.Contracts.Models;
 
@@ -8,5 +9,11 @@
     /// </summary>
     public interface IGameReadOnlyDatabase : IReadOnlyDatabase<IGame>
     {
+        /// <summary>
+        ///     Count the number of games that a  part of a game series.
+        /// </summary>
+        /// <param name="gameSeriesDocumentId">The document id of the game series.</param>
+        /// <returns>The document count.</returns>
+        Task<int> CountGames(string gameSeriesDocumentId);
     }
 }
--- nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Logic/GameDatabase.cs
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Logic/GameDatabase.cs
@@ -1,6 +1,8 @@
 ﻿namespace Md.Tga.Common.Firestore.Logic
 {
+    using System.Threading.Tasks;
     using Md.Common.Contracts.Model;
+    using Md.Common.Database;
     using Md.GoogleCloudFirestore.Logic;
     using Md.Tga.Common.Contracts.Models;
     using Md.Tga.Common.Firestore.Contracts.Logic;
@@ -18,6 +20,19 @@
         public GameDatabase(IRuntimeEnvironment runtimeEnvironment)
             : base(runtimeEnvironment, GameReadOnlyDatabase.CollectionName, Game.FromDictionary)
         {
+        }
+
+        /// <summary>
+        ///     Count the number of games that a  part of a game series.
+        /// </summary>
+        /// <param name="gameSeriesDocumentId">The document id of the game series.</param>
+        /// <returns>The document count.</returns>
+        public async Task<int> CountGames(string gameSeriesDocumentId)
+        {
+            var snapshot = await this.Collection()
+                .WhereEqualTo(DatabaseObject.ParentDocumentIdName, gameSeriesDocumentId)
+                .GetSnapshotAsync();
+            return snapshot.Count;
         }
     }
 }
