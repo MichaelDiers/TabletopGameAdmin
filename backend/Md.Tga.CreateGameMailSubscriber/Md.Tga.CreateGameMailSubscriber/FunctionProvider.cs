6dce466b6eb5bb98ca3f7d8ae5e25ee7156f46a5
MichaelDiers <md@sreid.de>
2022-04-15 23:21:29 +0200

    initial checkin

---
+++ backend/Md.Tga.CreateGameMailSubscriber/Md.Tga.CreateGameMailSubscriber/FunctionProvider.cs
@@ -0,0 +1,50 @@
+﻿namespace Md.Tga.CreateGameMailSubscriber
+{
+    using System.Threading.Tasks;
+    using Md.GoogleCloudFunctions.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Microsoft.Extensions.Logging;
+    using Surveys.Common.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Provider that handles the business logic of the cloud function.
+    /// </summary>
+    public class FunctionProvider : PubSubProvider<ICreateGameMailMessage, Function>
+    {
+        /// <summary>
+        ///     Access pub/sub for sending messages.
+        /// </summary>
+        private readonly ISendMailPubSubClient sendMailPubSubClient;
+
+        /// <summary>
+        ///     Creates a new instance of <see cref="FunctionProvider" />.
+        /// </summary>
+        /// <param name="logger">An error logger.</param>
+        /// <param name="sendMailPubSubClient">Send a new email.</param>
+        public FunctionProvider(ILogger<Function> logger, ISendMailPubSubClient sendMailPubSubClient)
+            : base(logger)
+        {
+            this.sendMailPubSubClient = sendMailPubSubClient;
+        }
+
+        /// <summary>
+        ///     Handles the pub/sub messages.
+        /// </summary>
+        /// <param name="message">The message that is handled.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        protected override async Task HandleMessageAsync(ICreateGameMailMessage message)
+        {
+            foreach (var gameSeriesPlayer in message.GameSeries.Players)
+            {
+                var sendMailMessage = new SendMailMessage(
+                    message.ProcessId,
+                    new[] {new Recipient(gameSeriesPlayer.Email, gameSeriesPlayer.Name)},
+                    new Recipient(message.GameSeries.Organizer.Email, message.GameSeries.Organizer.Name),
+                    "subject",
+                    new Body("html", "text"));
+                await this.sendMailPubSubClient.PublishAsync(sendMailMessage);
+            }
+        }
+    }
+}
---
+++ backend/Md.Tga.CreateGameMailSubscriber/Md.Tga.CreateGameMailSubscriber/Startup.cs
@@ -0,0 +1,34 @@
+namespace Md.Tga.CreateGameMailSubscriber
+{
+    using Google.Cloud.Functions.Hosting;
+    using Md.GoogleCloudFunctions.Contracts.Logic;
+    using Md.GoogleCloudPubSub.Contracts.Model;
+    using Md.Tga.Common.Contracts.Messages;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Surveys.Common.PubSub.Contracts.Logic;
+    using Surveys.Common.PubSub.Logic;
+
+    /// <summary>
+    ///     Initialize the function.
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="context">The builder context.</param>
+        /// <param name="services">Add services to this collection used in dependency injection context.</param>
+        public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection services)
+        {
+            var configuration = new FunctionConfiguration();
+            context.Configuration.Bind(configuration);
+
+            services.AddScoped<IPubSubClientEnvironment>(_ => configuration);
+            services.AddScoped<ISendMailPubSubClient, SendMailPubSubClient>();
+
+            services.AddScoped<IPubSubProvider<ICreateGameMailMessage>, FunctionProvider>();
+        }
+    }
+}
