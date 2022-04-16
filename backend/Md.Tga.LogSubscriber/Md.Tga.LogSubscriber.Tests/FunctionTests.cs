e05f3abbec97073da76e61a2176219b237befb2d
MichaelDiers <md@sreid.de>
2022-04-16 19:54:10 +0200

    log function

---
+++ backend/Md.Tga.LogSubscriber/Md.Tga.LogSubscriber.Tests/FunctionTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Md.Tga.SaveGameSubscriber.Tests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using CloudNative.CloudEvents;
+    using Google.Cloud.Functions.Testing;
+    using Google.Events.Protobuf.Cloud.PubSub.V1;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.Messages;
+    using Md.Tga.Common.Models;
+    using Md.Tga.Common.TestData.Generators;
+    using Md.Tga.Common.TestData.Mocks.Database;
+    using Md.Tga.Common.TestData.Mocks.PubSub;
+    using Newtonsoft.Json;
+    using Xunit;
+
+    /// <summary>
+    ///     Tests for <see cref="Function" />.
+    /// </summary>
+    public class FunctionTests
+    {
+        [Fact]
+        public async void HandleAsync()
+        {
+            var container = new TestDataContainer();
+            var gameSeries = container.GameSeries;
+            var game = container.Game;
+            var message = new SaveGameMessage(
+                Guid.NewGuid().ToString(),
+                gameSeries,
+                new Game(
+                    null,
+                    null,
+                    gameSeries.DocumentId,
+                    game.Name,
+                    game.GameTerminations));
+            await FunctionTests.HandleAsyncForMessage(message);
+        }
+
+        private static async Task HandleAsyncForMessage(ISaveGameMessage message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+            var data = new MessagePublishedData {Message = new PubsubMessage {TextData = json}};
+
+            var cloudEvent = new CloudEvent
+            {
+                Type = MessagePublishedData.MessagePublishedCloudEventType,
+                Source = new Uri("//pubsub.googleapis.com", UriKind.RelativeOrAbsolute),
+                Id = Guid.NewGuid().ToString(),
+                Time = DateTimeOffset.UtcNow,
+                Data = data
+            };
+
+            var logger = new MemoryLogger<Function>();
+            var provider = new FunctionProvider(logger, new GamesDatabaseMock(), new StartSurveyPubSubClientMock());
+            var function = new Function(logger, provider);
+            await function.HandleAsync(cloudEvent, data, CancellationToken.None);
+
+            Assert.Empty(logger.ListLogEntries());
+        }
+    }
+}
---
+++ backend/Md.Tga.LogSubscriber/Md.Tga.LogSubscriber/FunctionProvider.cs
@@ -0,0 +1,41 @@
+﻿namespace Md.Tga.SaveGameSubscriber
+{
+    using System;
+    using System.Threading.Tasks;
+    using Md.GoogleCloudFunctions.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.Firestore.Contracts.Logic;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    ///     Provider that handles the business logic of the cloud function.
+    /// </summary>
+    public class FunctionProvider : PubSubProvider<ILogMessage, Function>
+    {
+        /// <summary>
+        ///     Access the database.
+        /// </summary>
+        private readonly IGameDatabase database;
+
+        /// <summary>
+        ///     Creates a new instance of <see cref="FunctionProvider" />.
+        /// </summary>
+        /// <param name="logger">An error logger.</param>
+        /// <param name="database">Access to the database.</param>
+        public FunctionProvider(ILogger<Function> logger, IGameDatabase database)
+            : base(logger)
+        {
+            this.database = database ?? throw new ArgumentNullException(nameof(database));
+        }
+
+        /// <summary>
+        ///     Handles the pub/sub messages.
+        /// </summary>
+        /// <param name="message">The message that is handled.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        protected override async Task HandleMessageAsync(ILogMessage message)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
