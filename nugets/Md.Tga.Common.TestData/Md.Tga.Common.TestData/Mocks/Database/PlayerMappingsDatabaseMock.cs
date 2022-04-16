61201ba5cabc17b489814b4a531617ede1cdd69a
MichaelDiers <md@sreid.de>
2022-04-16 09:52:26 +0200

    add mock

---
+++ nugets/Md.Tga.Common.TestData/Md.Tga.Common.TestData/Mocks/Database/PlayerMappingsDatabaseMock.cs
@@ -0,0 +1,21 @@
+﻿namespace Md.Tga.Common.TestData.Mocks.Database
+{
+    using System.Collections.Generic;
+    using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Firestore.Contracts.Logic;
+    using Md.Tga.Common.Models;
+
+    public class PlayerMappingsDatabaseMock : DatabaseMock<IPlayerMappings>, IPlayerMappingsDatabase
+    {
+        public PlayerMappingsDatabaseMock()
+            : this(new Dictionary<string, IPlayerMappings>())
+        {
+        }
+
+        public PlayerMappingsDatabaseMock(IDictionary<string, IPlayerMappings> dictionary)
+            : base(dictionary, PlayerMappings.FromDictionary)
+
+        {
+        }
+    }
+}
