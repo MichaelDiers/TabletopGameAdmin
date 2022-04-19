c21b340c7e55e59be1c5fb2b6a878278260c01df
MichaelDiers <md@sreid.de>
2022-04-19 16:00:41 +0200

    add db for termination surveys

---
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Contracts/Logic/IGameTerminationSurveyReadOnlyDatabase.cs
@@ -0,0 +1,12 @@
+﻿namespace Md.Tga.Common.Firestore.Contracts.Logic
+{
+    using Md.GoogleCloudFirestore.Contracts.Logic;
+    using Md.Tga.Common.Contracts.Models;
+
+    /// <summary>
+    ///     Database operations of game termination surveys collection.
+    /// </summary>
+    public interface IGameTerminationSurveyReadOnlyDatabase : IReadOnlyDatabase<IGameTerminationSurvey>
+    {
+    }
+}
---
+++ nugets/Md.Tga.Common.Firestore/Md.Tga.Common.Firestore/Logic/GameTerminationSurveyDatabase.cs
@@ -0,0 +1,26 @@
+﻿namespace Md.Tga.Common.Firestore.Logic
+{
+    using Md.Common.Contracts.Model;
+    using Md.GoogleCloudFirestore.Logic;
+    using Md.Tga.Common.Contracts.Models;
+    using Md.Tga.Common.Firestore.Contracts.Logic;
+    using Md.Tga.Common.Models;
+
+    /// <summary>
+    ///     Database for <see cref="GameTerminationSurvey" /> collections,
+    /// </summary>
+    public class GameTerminationSurveyDatabase : Database<IGameTerminationSurvey>, IGameTerminationSurveyDatabase
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="GameTerminationSurveyDatabase" />.
+        /// </summary>
+        /// <param name="runtimeEnvironment">The runtime environment.</param>
+        public GameTerminationSurveyDatabase(IRuntimeEnvironment runtimeEnvironment)
+            : base(
+                runtimeEnvironment,
+                GameTerminationSurveyReadOnlyDatabase.CollectionName,
+                GameTerminationSurvey.FromDictionary)
+        {
+        }
+    }
+}
