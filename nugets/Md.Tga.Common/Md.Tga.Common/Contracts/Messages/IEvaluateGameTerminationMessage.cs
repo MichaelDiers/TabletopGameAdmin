cee8a8bcf67f39c7d4708764cc0bb89dfeb6f3e7
MichaelDiers <md@sreid.de>
2022-04-20 18:09:46 +0200

    evaluate game termination message

---
+++ nugets/Md.Tga.Common/Md.Tga.Common/Contracts/Messages/IEvaluateGameTerminationMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Md.Tga.Common.Contracts.Messages
+{
+    using Md.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Start the evaluation of a game.
+    /// </summary>
+    public interface IEvaluateGameTerminationMessage : IMessage
+    {
+        /// <summary>
+        ///     Gets the id of the game document.
+        /// </summary>
+        string GameDocumentId { get; }
+    }
+}
---
+++ nugets/Md.Tga.Common/Md.Tga.Common/Messages/EvaluateGameTerminationMessage.cs
@@ -0,0 +1,30 @@
+﻿namespace Md.Tga.Common.Messages
+{
+    using Md.Common.Extensions;
+    using Md.Common.Messages;
+    using Md.Tga.Common.Contracts.Messages;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    ///     Start the evaluation of a game.
+    /// </summary>
+    public class EvaluateGameTerminationMessage : Message, IEvaluateGameTerminationMessage
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="EvaluateGameTerminationMessage" />.
+        /// </summary>
+        /// <param name="processId">The global process id.</param>
+        /// <param name="gameDocumentId">The document id of the game.</param>
+        public EvaluateGameTerminationMessage(string processId, string gameDocumentId)
+            : base(processId)
+        {
+            this.GameDocumentId = gameDocumentId.ValidateIsAGuid(nameof(gameDocumentId));
+        }
+
+        /// <summary>
+        ///     Gets the id of the game document.
+        /// </summary>
+        [JsonProperty("gameId", Required = Required.Always, Order = 11)]
+        public string GameDocumentId { get; }
+    }
+}
