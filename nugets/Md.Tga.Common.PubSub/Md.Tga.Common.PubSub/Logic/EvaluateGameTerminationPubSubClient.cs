064bbc6de1292eecd29ebc09e7cb13a3be17d11c
MichaelDiers <md@sreid.de>
2022-04-20 18:14:05 +0200

    evaluate game termination message

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/EvaluateGameTerminationPubSubClient.cs
@@ -0,0 +1,23 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Contracts.Model;
+    using Md.GoogleCloudPubSub.Logic;
+    using Md.Tga.Common.Contracts.Messages;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IEvaluateGameTerminationMessage" />.
+    /// </summary>
+    public class EvaluateGameTerminationPubSubClient
+        : AbstractPubSubClient<IEvaluateGameTerminationMessage>, IEvaluateGameTerminationPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="EvaluateGameTerminationPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public EvaluateGameTerminationPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
