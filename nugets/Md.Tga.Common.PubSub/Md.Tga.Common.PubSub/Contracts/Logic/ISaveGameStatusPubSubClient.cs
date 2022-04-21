fcdfaa8e3aed79384d9d9f5c43685379182f3b1e
MichaelDiers <md@sreid.de>
2022-04-21 09:47:27 +0200

    save game status

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Contracts/Logic/ISaveGameStatusPubSubClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Md.Tga.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Md.Tga.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISaveGameStatusMessage" />.
+    /// </summary>
+    public interface ISaveGameStatusPubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="ISaveGameStatusMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        // ReSharper disable once UnusedMember.Global
+        Task PublishAsync(ISaveGameStatusMessage message);
+    }
+}
