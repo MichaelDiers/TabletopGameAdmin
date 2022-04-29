7b1211b881188397ae6c17b5f12a6016a87cda7e
MichaelDiers <md@sreid.de>
2022-04-29 14:32:12 +0200

    scheduler message

---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Contracts/Logic/ISchedulerPubSubClient.cs
@@ -0,0 +1,18 @@
+﻿namespace Md.Tga.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Md.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     A client for publishing messages to a scheduler.
+    /// </summary>
+    public interface ISchedulerPubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="IMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        Task PublishAsync(IMessage message);
+    }
+}
---
+++ nugets/Md.Tga.Common.PubSub/Md.Tga.Common.PubSub/Logic/SchedulerPubSubClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Md.Tga.Common.PubSub.Logic
+{
+    using Md.Common.Contracts.Messages;
+    using Md.GoogleCloudPubSub.Contracts.Model;
+    using Md.GoogleCloudPubSub.Logic;
+    using Md.Tga.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IMessage" />.
+    /// </summary>
+    public class SchedulerPubSubClient : AbstractPubSubClient<IMessage>, ISchedulerPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="SchedulerPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public SchedulerPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
