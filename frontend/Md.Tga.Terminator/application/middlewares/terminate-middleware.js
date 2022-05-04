a23459e52717f4161cec9c688eb5bf07dcb9c66b
MichaelDiers <md@sreid.de>
2022-05-04 18:50:44 +0200

    middleware fix

--- frontend/Md.Tga.Terminator/application/middlewares/terminate-middleware.js
+++ frontend/Md.Tga.Terminator/application/middlewares/terminate-middleware.js
@@ -39,7 +39,7 @@ const initialize = (config = {}) => {
     isUuid(body('terminationId')),
     isUuid(body('winningSideId')),
     body('reason').escape(),
-    body('rounds').exists().isInt({gt: 0, lt: 40 }),
+    body('rounds').exists().isInt({gt: -1, lt: 40 }),
     middlewareFunc,
   );
 
